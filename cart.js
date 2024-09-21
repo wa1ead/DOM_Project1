ac6232ca7aabf20987c8c999426bf8ab2c5a3d97
wa1ead <waliddraa2@gmail.com>
2024-09-21 14:22:23 +0100

    convert cartItem to array: DOM

--- cart.js
+++ cart.js
@@ -1,9 +1,10 @@
-var cartContainer = document.querySelectorAll(".cart-items");
-console.log(cartContainer);
 var cartItem = document.getElementsByClassName("cart-item");
-console.log(cartItem);
-Array.from(cartItem).forEach((item) => {
+cartItemArray = Array.from(cartItem);
+
+cartItemArray.forEach((item) => {
   console.log(item);
   var price = item.querySelector(".item-price p").textContent;
   console.log(price);
+  var quantity = item.querySelector(".item-quantity input").value;
+  console.log(quantity);
 });
