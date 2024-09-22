e8d0dec4673e4f4f4ec2b5bdbfd3531a5ab22137
wa1ead <waliddraa2@gmail.com>
2024-09-22 12:36:18 +0100

    convert price string into number: DOM

--- cart.js
+++ cart.js
@@ -3,8 +3,12 @@ cartItemArray = Array.from(cartItem);
 
 cartItemArray.forEach((item) => {
   console.log(item);
-  var price = item.querySelector(".item-price p").textContent;
+  var price = Number(
+    item.querySelector(".item-price p").textContent.replace("$", "")
+  );
   console.log(price);
   var quantity = item.querySelector(".item-quantity input").value;
   console.log(quantity);
+  var total = price * quantity;
+  console.log(total);
 });
